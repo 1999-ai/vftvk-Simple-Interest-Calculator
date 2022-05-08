05413eb43615ce02863277c5dd29ea6f0dda911e
1999-ai <82260325+1999-ai@users.noreply.github.com>
2022-05-07 22:03:44 -0700

    finshed

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ function compute()
     let now = new Date().getFullYear() + 10;
     let result ="";
     let principal = document.getElementById("principal").value;
+    if (principal <= 0){
+        alert("please enter a positive number");
+        aocument.getElementById("principal").focus();
+    }
     result += "If you deposite <return>" + principal + "</return>,<br>";
     let rates = document.getElementById("rate").value;
     result += "at an interest rate of <return>" + rates + "%</return>.<br>";
     let years = document.getElementById("years").value;
-    let amount = Math.floor(principal * (1 + rates / 100) ** years);
+    let amount = Math.floor(principal * ( rates / 100) * years );
     result += "You will receive an amount of <return>" + amount +
     "</return>,<br>in the year <return>"+ now + "</return>";
     console.log(result);
